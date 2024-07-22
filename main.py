8b463fdc6c0be514735abe09fd223b2c5b70a69d
Vincenzo Lucido <vin.lucido@berkeley.edu>
2024-07-22 14:06:25 -0700

    options

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
+import re
 
 def main():
-    print(os.listdir('user_info/ranking'))
+    
+    ranking_options = [option[:-4] for option in os.listdir('user_info/ranking')]
+    
+    print(ranking_options)
 
 if __name__ == "__main__":
     main()
