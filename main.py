19f744c4f049aa75e563f3a1ae9753309cd7990f
Vincenzo Lucido <vin.lucido@berkeley.edu>
2024-07-22 14:18:38 -0700

    jobs and user_info options are being reliably identified

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import os
 import re
 
 def main():
-    
-    ranking_options = [option[:-4] for option in os.listdir('user_info/ranking')]
-    
+    job_options = [option[:-4] for option in os.listdir('fill_me_out/job_info') if option.endswith('.txt')]
+    print(job_options)
+
+    ranking_options = [option[:-4] for option in os.listdir('fill_me_out/user_info/ranking') if option.endswith('.txt')]
+
     print(ranking_options)
 
 if __name__ == "__main__":
