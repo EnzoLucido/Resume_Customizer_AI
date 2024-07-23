2add3ca819072892f99258e984cb9291e5f332af
Vincenzo Lucido <vin.lucido@berkeley.edu>
2024-07-22 22:47:19 -0700

    added an all function!

--- main.py
+++ main.py
@@ -3,6 +3,41 @@ import model
 import os
 import re
 
+def ranking_section():
+    ranking_options = [option[:-4] for option in os.listdir('fill_me_out/user_info/ranking') if option.endswith('.txt')]
+    ranking_options.append("all")
+
+    print("Please pick a skill to rank")
+
+    user_input = input("You: ")
+
+    while not user_input in ranking_options:
+        print("Please try again, pick one of these values: " + str(ranking_options))
+        user_input = input("You: ")
+
+    ranking = user_input
+    print("Groovy, we will be ranking your " + ranking + ".")
+    
+    if ranking == "all":
+        ranking_all(ranking_options)
+
+
+    model.rank(ranking)
+
+    print("Do you want to rank another section? Reply yes or no")
+    
+    user_input = input("You: ")
+    while not user_input in ["yes", "no"]:
+        print("Please reply yes or no")
+        user_input = input("You: ")
+    if user_input == "yes":
+        ranking_section()
+
+def ranking_all(ranking_options):
+    for option in ranking_options:
+        if not option == "all":
+            model.rank(option)
+
 def main():
     job_options = [option[:-4] for option in os.listdir('fill_me_out/job_info') if option.endswith('.txt')]
     print("Your current job application options are: " + str(job_options))
@@ -29,16 +64,7 @@ def main():
 
     print("Please pick a skill to rank")
 
-    user_input = input("You: ")
-
-    while not user_input in ranking_options:
-        print("Please try again, pick one of these values: " + str(ranking_options))
-        user_input = input("You: ")
-
-    ranking = user_input
-    print("Groovy, we will be ranking your " + ranking + ".")
-
-    model.rank(ranking)
+    ranking_section()
 
 if __name__ == "__main__":
     main()
