b2625763cb505a6aacb657c0b4e374fb9a8a3821
Vincenzo Lucido <vin.lucido@berkeley.edu>
2024-07-24 11:56:09 -0700

    working on more files

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def ranking_section():
     user_input = input("You: ")
 
     while not user_input in ranking_options:
-        print("Please try again, pick one of these values: " + str(ranking_options))
+        print("Please try again, pick one of these values: " + str(ranking_options) + "\n")
         user_input = input("You: ")
 
     ranking = user_input
@@ -23,25 +23,31 @@ def ranking_section():
 
 
     model.rank(ranking)
-
-    print("Do you want to rank another section? Reply yes or no")
+    print("\n")
+    print("Do you want to modify these skills, continue with another section or quit? Reply modify, continue or quit\n")
     
     user_input = input("You: ")
-    while not user_input in ["yes", "no"]:
-        print("Please reply yes or no")
+    while not user_input in ["modify", "continue", "quit"]:
+        print("Please reply modify, continue or quit\n")
         user_input = input("You: ")
-    if user_input == "yes":
+    if user_input == "modify":
+        modify()
+    elif user_input == "yes":
         ranking_section()
+
+def modify():
+
 
 def ranking_all(ranking_options):
     for option in ranking_options:
         if not option == "all":
             model.rank(option)
+    print("\n")
 
 def main():
     job_options = [option[:-4] for option in os.listdir('fill_me_out/job_info') if option.endswith('.txt')]
-    print("Your current job application options are: " + str(job_options))
-    print("Please choose which job application you would like to work on: ")
+    print("Your current job application options are: " + str(job_options) + "\n")
+    print("Please choose which job application you would like to work on: \n")
 
     user_input = input("You: ")
 
@@ -55,7 +61,7 @@ def main():
     job = user_input
 
     print("Awesome, we are working on your " + job + " application.")
-    print("Here's the skills I have identified!")
+    print("Please wait while I identify skills")
     
     model.create_pick_out_skills_model()
     model.pick_out_skills(job)
